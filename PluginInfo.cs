0d9728d245ea6283ff383b752e433f37b7505d33
ImudTrust <nightware.win@gmail.com>
2026-03-13 19:42:23 +0100

    Uploaded ig

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace CXS
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.imudtrust.gorillatag.CXS";
+        public const string Name = "CXS";
+        public const string Version = "1.0.0";
+    }
+}
