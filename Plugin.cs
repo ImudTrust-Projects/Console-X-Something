285196a7ff7fcce554e50b78c30fd5a9f8727960
imudtust <nightware.win@gmail.com>
2026-05-15 21:39:36 +0100

    added skate park & monke blocks

---
+++ Plugin.cs
@@ -0,0 +1,11 @@
+﻿using BepInEx;
+
+namespace CXS
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        // Put this snippet of code in your BaseUnityPlugin
+        void Start() => CXS.LoadCXS();
+    }
+}
